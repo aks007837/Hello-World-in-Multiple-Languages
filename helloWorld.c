bc80b50cfbcb3a1e96de31bac9bc30dcd1996e63
Sinky Priya <91044616+20sinkypriya@users.noreply.github.com>
2024-10-22 00:22:17 +0530

    Create helloWorld.c
    Create helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,20 @@
+Here’s the code to print "Hello, World!" in C language:
+
+###  C Code to Print "Hello, World!"
+#include <stdio.h>  // This includes the standard input-output library.
+
+int main() {
+    // The main function is where the program starts executing.
+
+    printf("Hello, World!\n");
+    // printf is a function used to output text to the console.
+    // "\n" is used to move to a new line after printing the message.
+
+    return 0;
+    // Return 0 indicates that the program ended successfully.
+}
+
+###  Example Output:
+```
+Hello, World!
+```
