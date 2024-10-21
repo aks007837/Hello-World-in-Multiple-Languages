2b59530e99068c0d27849bee05e79c6984bce649
Sinky Priya <91044616+20sinkypriya@users.noreply.github.com>
2024-10-21 23:42:23 +0530

    Create helloWorld.cpp
    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,21 @@
+Program prints "Hello, World!" to the console of cpp.
+
+### Code
+#include <iostream>  // This library is used for input-output operations.
+
+int main() {
+    // The main function is the entry point of the C++ program.
+
+    std::cout << "Hello, World!" << std::endl;
+    // std::cout is used to output data to the console.
+    // The << operator is used to send the string "Hello, World!" to the console.
+    // std::endl ends the line and flushes the output buffer.
+
+    return 0;
+    // Return 0 to indicate that the program has successfully executed.
+}
+
+### Example Output:
+```
+Hello, World!
+```
